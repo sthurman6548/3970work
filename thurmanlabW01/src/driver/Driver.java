463a28e1a8cb69a828f42ae477a73cb0fbaa5c55
Seth <sthurman403@gmail.com>
2026-01-13 16:23:30 -0600

    chore(update): added one line to labw01

--- thurmanlabW01/src/driver/Driver.java
+++ thurmanlabW01/src/driver/Driver.java
@@ -3,6 +3,6 @@ package driver;
 public class Driver {
 public static void main(String[] args) {
 	System.out.println("Hello World");
-	System.out.println("Hello from VS code");
+	System.out.println("Hello from VS code.");
 	}
 }
